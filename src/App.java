b256219d29826571e1d075d7ed97a664d1a45c8f
Jakob <SpAyS@users.noreply.github.com>
2021-11-23 15:38:02 +0100

    Created

---
+++ src/App.java
@@ -0,0 +1,7 @@
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Opgave1 opg1 = new Opgave1();
+        opg1.RigtigRegning();
+    }
+}
---
+++ src/Opgave1.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class Opgave1 {
+
+    public void RigtigRegning() {
+        int a, b, c, userInput;
+
+        a = (int)(Math.random() * 22);
+        b = (int)(Math.random() * 22);
+        c = (int)(Math.random() * 4);
+        
+        switch (c) {
+            case 1:
+                System.out.println("Hvad er " + a + " + " + b + "?");
+                userInput = UserInputValidate();
+                if (userInput == a + b)
+                    System.out.println("Du har regnet rigtigt!");
+                else
+                    System.out.println("Du har regnet forkert.");
+            break;
+            case 2:
+                System.out.println("Hvad er " + a + " - " + b + "?");
+                userInput = UserInputValidate();
+                if (userInput == a - b)
+                    System.out.println("Du har regnet rigtigt!");
+                else
+                    System.out.println("Du har regnet forkert.");
+            break;
+            case 3:
+                System.out.println("Hvad er " + a + " * " + b + "?");
+                userInput = UserInputValidate();
+                if (userInput == a * b)
+                   System.out.println("Du har regnet rigtigt!");
+                else
+                    System.out.println("Du har regnet forkert.");
+            break;
+            default:
+                System.out.println("Der skete en fejl.");
+            break;
+        }
+    }
+
+    public int UserInputValidate() {
+        String userInput;
+        int parsedInt = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Svar: ");
+            userInput = scanner.next().trim();
+            try {
+                parsedInt = Integer.parseInt(userInput);
+                break;
+            } catch (Exception e) {
+                System.out.println("Du skal skrive et hel tal.");
+            }
+        }
+        scanner.close();
+        return parsedInt;
+    }
+
+}
